dc5a4c8a177817dab42fd62a2c7bf5e46981be69
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-07-07 21:33:09 -0500

    oh wait nvm im not dum

--- cogs/dev.py
+++ cogs/dev.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from jishaku.codeblocks import Codeblock, codeblock_converter
 
 
-class REPL(commands.Cog):
+class Dev(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
