26e4d48c9781d7a1ffdf65268f8b7f79112e933b
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-07-21 02:24:50 -0500

    errors pt 14

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,35 +1,52 @@
-import discord
+import discord, random
 from cogs import guild
 from discord.ext import commands
-from discord.ext.commands.core import command
 
 class Errors(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def e(self, ctx):
+        pass
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx:commands.Context, error:Exception):
+        '''The command handler'''
+
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.message.add_reaction('🚫')
+            return
+
         parent_name = ' '.join(ctx.invoked_parents)
         if parent_name != '': parent_name += ' '
-        try:
-            await ctx.message.add_reaction('🚫')
-            await ctx.bot.get_channel(867254023617642497).send(
-                embed=discord.Embed(
-                    color=discord.Color.red(), 
-                    title=f"Error running {''.join(ctx.invoked_parents)} {ctx.invoked_with}:", 
-                    description=f"```{error}```\n"
-                    f"Author: {ctx.author} ({ctx.author.id})\n"
-                    f"Channel: {ctx.channel} ({ctx.channel.id})\n"
-                    f"Guild: {ctx.guild} ({ctx.guild.id})\n"
-                    f"Full message: ```{ctx.message.content}```\n", 
-                    timestamp=ctx.message.created_at
-                ).set_author(
-                    name=ctx.author.name, 
-                    icon_url=ctx.author.avatar_url
+
+        if not type(error) in [
+            commands.CommandNotFound,
+            commands.CommandOnCooldown,
+        ]:
+            try:
+                await ctx.message.add_reaction('🚫')
+                await ctx.bot.get_channel(867254023617642497).send(
+                    embed=discord.Embed(
+                        color=discord.Color.red(), 
+                        title=f"Error running {''.join(ctx.invoked_parents)} {ctx.invoked_with}:", 
+                        description=f"```{error}```\n"
+                        f"Author: {ctx.author} ({ctx.author.id})\n"
+                        f"Channel: {ctx.channel} ({ctx.channel.id})\n"
+                        f"Guild: {ctx.guild} ({ctx.guild.id})\n"
+                        f"Full message: ```{ctx.message.content}```\n", 
+                        timestamp=ctx.message.created_at
+                    ).set_author(
+                        name=ctx.author.name, 
+                        icon_url=ctx.author.avatar_url
+                    )
                 )
-            )
-        except:
+            except:
             pass
+
         if isinstance(error, commands.CommandNotFound):
             await ctx.reply(f"Command not found.")
         elif isinstance(error, commands.MissingRequiredArgument):
@@ -80,6 +97,15 @@ class Errors(commands.Cog):
         elif isinstance(error, commands.NoPrivateMessage):
             await ctx.reply(
                 embed=discord.Embed(title=f"This command can't be used in private messages", 
+                color=0xff0000,
+                description=f"**Usage:**\n{ctx.prefix}{parent_name}{ctx.invoked_with} {ctx.command.signature}"
+                ).set_footer(
+                    text=f"Type {ctx.prefix}help {parent_name}{ctx.invoked_with} for more info"
+                )
+            )
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.reply(
+                embed=discord.Embed(title=f"This command is on cooldown", 
                 color=0xff0000,
                 description=f"**Usage:**\n{ctx.prefix}{parent_name}{ctx.invoked_with} {ctx.command.signature}"
                 ).set_footer(
