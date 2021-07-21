1ad620685bce873c9b9d65d3d8ed50128cc2eeef
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-07-21 01:12:57 -0500

    errors pt 9

--- cogs/errors.py
+++ cogs/errors.py
@@ -47,6 +47,15 @@ class Errors(commands.Cog):
                     text=f"Type {ctx.prefix}help {ctx.invoked_with} for more info"
                 )
             )
+        elif isinstance(error, guild.NotInGuild):
+            await ctx.reply(
+                embed=discord.Embed(title=f"You're not in the correct guild.", 
+                color=0xff0000,
+                description=f"**Usage:**\n{ctx.prefix}{ctx.invoked_with} {ctx.command.signature}"
+                ).set_footer(
+                    text=f"Type {ctx.prefix}help {ctx.invoked_with} for more info"
+                )
+            )
         elif isinstance(error, commands.CheckFailure):
             await ctx.reply(
                 embed=discord.Embed(title=f"You don't have permission to use this command", 
