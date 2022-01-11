cd86823a051ea7d6d45bf973bcd238492c634f3b
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-11 12:17:59 -0500

    fixed the emoji cutoff

--- cogs/fun.py
+++ cogs/fun.py
@@ -58,7 +58,10 @@ class Fun(commands.Cog):
             Example:
                 `;squaredraw (4x(4xkw);(4xwk);)`
         '''
-        return await ctx.send(embed=discord.Embed(title='Here\'s your monster', description='\n'.join(lines)))
+        text = "\n".join(lines)
+        if len("".join(lines)) > 200:
+            text = f"```\n{text}```"
+        return await ctx.send(embed=discord.Embed(title='Here\'s your monster', description=text))
 
     @commands.command(aliases=["pop", "bubble", "bubblewrap"])
     async def bubbles(self, ctx, width:int, height:int):
