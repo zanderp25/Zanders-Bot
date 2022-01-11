1a0ba9f6d4a182f5d676de2090202e1dec5deb88
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-11 08:38:14 -0500

    added help to fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -32,10 +32,43 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def squaredraw(self, ctx, *lines: SquareDraw):
+        '''
+            Draws an image with emoji squares.
+            Made by Clari (💜Clari#7744)
+
+            How to use:
+                Use a letter below to represent the color of each square, and a number following the letter
+                to represent the number of squares of that color. The number of squares is optional.
+                Example:
+                    `k2r3k2`
+                You can also use groups, like this:
+                    `(3x(k2r3k2))`
+
+            Colors:
+                - k - black
+                - w - white
+                - r - red
+                - o - orange
+                - y - yellow
+                - g - green
+                - p - purple
+                - n - brown
+                - ; - newline (or space)              
+            
+            Example:
+                `;squaredraw (4x(4xkw);(4xwk);)`
+        '''
         return await ctx.send(embed=discord.Embed(title='Here\'s your monster', description='\n'.join(lines)))
 
     @commands.command()
     async def bubbles(self, ctx, width:int, height:int):
+        '''
+            Shows you virtual bubble wrap with a given width and height.
+            Made by Clari (💜Clari#7744)
+
+            How to use:
+                `;bubbles <width> <height>`
+        '''
         width = width if 0<width<=10 else 10
         height = height if 0<height <=20 else 20
         await ctx.send((('pop'*width)+'\n')*height)
