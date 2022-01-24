8924cefaf951f75a5954d1749815532164fd1271
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-24 12:32:45 -0500

    Added UserNotFound error

--- cogs/errors.py
+++ cogs/errors.py
@@ -64,6 +64,15 @@ class Errors(commands.Cog):
                     text=f"Type {ctx.prefix}help {parent_name}{ctx.invoked_with} for more info"
                 )
             )
+        elif isinstance(error, commands.UserNotFound):
+            await ctx.reply(
+                embed=discord.Embed(title=f"The user does not exist.", 
+                color=0xff0000,
+                description=f"**Usage:**\n{ctx.prefix}{parent_name}{ctx.invoked_with} {ctx.command.signature}"
+                ).set_footer(
+                    text=f"Type {ctx.prefix}help {parent_name}{ctx.invoked_with} for more info"
+                )
+            )
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.reply(
                 embed=discord.Embed(title=f"{parent_name}{ctx.invoked_with} is missing an argument", 
