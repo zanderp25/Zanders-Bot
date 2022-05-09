29c9a23b89d1ed50fce3df91155ca88f10d35308
zanderp25 <zander@zanderp25.com>
2022-05-08 22:45:58 -0400

    no need to reload when it does it for you

--- cogs/dev.py
+++ cogs/dev.py
@@ -198,7 +198,6 @@ class Dev(commands.Cog):
     @commands.is_owner()
     async def ttt_pull(self, ctx):
         await self.bot.cogs["Jishaku"].jsk_shell(ctx,argument=codeblock_converter("pm2 pull 9"))
-        await self.bot.cogs["Jishaku"].jsk_shell(ctx,argument=codeblock_converter("pm2 reload 9"))
 
 def setup(bot):
     bot.add_cog(Dev(bot))
