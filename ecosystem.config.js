89b9d9d70f7f604fc6fa49024a1572839789b8f4
zanderp25 <zander@zanderp25.com>
2023-04-22 11:44:01 -0400

    made a pm2 environment file

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+        name       : "Zander's Bot",
+        script     : "./bot.py",
+        interpreter: "python3",
+        cwd        : "/home/zanderp25/bots/Zanders-Bot",
+    }]
+}
